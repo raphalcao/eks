6b72de894698d4058ac00065f1a64d005a86fa2e
raphalcao <raphalcao@gmail.com>
2025-02-01 06:18:46 -0300

    fix

--- terraform-eks/providers.tf
+++ terraform-eks/providers.tf
@@ -3,20 +3,3 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  default     = ""
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  default     = ""
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
